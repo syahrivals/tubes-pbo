a18bdee48f069fe9c259f184a8f68c04f583a952
YENS <alfiannary09@gmail.com>
2025-12-18 00:14:41 +0700

    Fix test database compatibility issue

--- src/test/java/com/example/tubes_pbo/TubesPboApplicationTests.java
+++ src/test/java/com/example/tubes_pbo/TubesPboApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.tubes_pbo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TubesPboApplicationTests {
 
 	@Test

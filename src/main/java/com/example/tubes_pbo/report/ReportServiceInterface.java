31f7fc3621bf81845ed011b619f721f82cfcfcb8
YENS <alfiannary09@gmail.com>
2025-12-29 11:50:48 +0700

    progress tubes pbo

--- src/main/java/com/example/tubes_pbo/report/ReportServiceInterface.java
+++
@@ -1,9 +0,0 @@
-package com.example.tubes_pbo.report;
-
-import java.io.File;
-import java.util.Map;
-
-public interface ReportServiceInterface {
-    File generateReport(Map<String, String> data);
-}
-
